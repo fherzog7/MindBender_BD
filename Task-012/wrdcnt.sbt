b0e2bf0f7a781da5ca131e4ed5430e7406d2ab53
fherzog7 <frederick.herzog@enhanceit.us>
2020-11-17 23:46:11 -0500

    Task--012

---
+++ Task-012/wrdcnt.sbt
@@ -0,0 +1,8 @@
+name := "Word Count"
+
+version := "1.0"
+
+scalaVersion := "2.13.0"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.0.2"
+
---
+++ Task-012/wrdcnt.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SparkWordCount {
+  def main(args: Array[String]) {
+    // create Spark context with Spark configuration
+    val sc = new SparkContext(new SparkConf()
+        .setAppName("Spark Count")
+        .setMaster("local[*]"))
+    
+    val data = sc.textFile("/home/eit/Shakespeare.txt")
+        .flatMap(line => line.split(" "))
+        .map(word => (word, 1))
+
+    var counts = map.reduceByKey(_ + _)
+
+
+    //val splitdata = data.flatMap(line => line.split(" "))
+    //val mapdata = splitdata.map(word => (word,1))
+    //val reducedata = mapdata.reduceByKey(_+_)
+
+    counts.saveAsTextFile("/home/eit/sparkout")
+
+    sc.stop()
+
+
+  }
+}
+
+
+//spark-submit \
+ //--class "wrdcnt.scala" \
+ //   --master local[4] \
+ // target/scala-2.10/word-count_2.13-1.0.jar
