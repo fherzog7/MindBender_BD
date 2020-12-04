e3fc9b601f8ef9a2c5e96a8f9fc805d5c3a4dfe2
fherzog7 <frederick.herzog@enhanceit.us>
2020-12-03 23:04:27 -0500

    Updated Consumer for hbase integration

--- Task-018/CoinConsumer/build.sbt
+++ Task-018/CoinConsumer/build.sbt
@@ -6,6 +6,10 @@ scalaVersion := "2.11.8"
 
 val sparkVersion = "2.2.0"
 
+val hadoopVersion = "2.7.3"
+
+val hbaseVersion = ""
+
 libraryDependencies ++= Seq(
 
     "org.apache.spark" %% "spark-core" % sparkVersion,
--- Task-018/CoinConsumer/src/main/scala/CoinConsumer.scala
+++ Task-018/CoinConsumer/src/main/scala/CoinConsumer.scala
@@ -2,6 +2,9 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger  
+
+import org.apache.spark.sql.datasources.hbase.HBaseTableCatalog
 
 object CoinConsumer
 {
@@ -9,21 +12,41 @@ object CoinConsumer
     def main(args: Array[String])
     {
 
-      val schema =
+      val rawSchema =
    		StructType(List(
      	StructField("status", StringType, true),
      	StructField("data", StringType, true) ) )
 
-   	  val schema2 =
+   	  val dataSchema =
    		StructType(List(
      	StructField("base", StringType, true),
      	StructField("coin", StringType, true) ) )
 
-   	  val schema3 =
+   	  val coinSchema =
    		StructType(List(
      	StructField("id", StringType, true),
      	StructField("symbol", StringType, true),
-     	StructField("price", StringType, true) ) )
+      StructField("name", StringType, true),
+     	StructField("price", IntegerType, true),
+      StructField("volume", IntegerType, true),
+      StructField("totalSupply", StringType, true),
+      StructField("rank", StringType, true)) )
+
+      def catalog =
+        s"""{
+           |"table":{"namespace":"default", "name":"coins"},
+           |"rowkey":"id",
+           |"columns":{
+           |"id":{"cf":"rowkey", "col":"id", "type":"string"},
+           |"symbol":{"cf":"general", "col":"symbol", "type":"string"},
+           |"name":{"cf":"general", "col":"name", "type":"string"},
+           |"price":{"cf":"tech", "col":"middleName", "type":"double"},
+           |"volume":{"cf":"tech", "col":"addressLine", "type":"integer"},
+           |"totalSupply":{"cf":"tech", "col":"totalSupply", "type":"integer"},
+           |"rank":{"cf":"general", "col":"rank", "type":"string"},
+           |"timestamp":{"cf":"general", "col":"timestamp", "type":"string"}
+           |}
+           |}""".stripMargin
 
       val ss = SparkSession
       	.builder
@@ -33,7 +56,7 @@ object CoinConsumer
 
       import ss.implicits._
 
-      val inputDf = ss
+    val inputDf = ss
       	.readStream
       	.format("kafka")
       	.option("kafka.bootstrap.servers", "localhost:9099")
@@ -41,22 +64,32 @@ object CoinConsumer
       	.load()
 
 	  val rawDataframe = inputDf.select($"value" cast "string" as "json")
-	  	.select(from_json($"json", schema) as "stuff")
+	  	.select(from_json($"json", rawSchema) as "stuff")
 	  	.select("stuff.*")
 	  	.drop("status")  
 
-	  val coin = rawDataframe.select(from_json($"data", schema2) as "stuff")
+	  val coin = rawDataframe.select(from_json($"data", dataSchema) as "stuff")
 	  	.select("stuff.*")
 	  	.drop("base")
-	  	.select(from_json($"coin", schema3) as "coin")
+	  	.select(from_json($"coin", coinSchema) as "coin")
 	  	.select("coin.*")
+      .withColumn("timestamp", lit(System.currentTimeMillis))
+
+      coin.printSchema()
+
+      coin.write
+        .options(Map(HBaseTableCatalog.tableCatalog -> catalog, 
+            HBaseTableCatalog.newTable -> 4))
+        .format("org.apache.spark.sql.execution.datasources.hbase")
+        .save()
 
       val query = coin
       	.writeStream
       	.outputMode("Append")
       	.format("console")
+        .trigger(Trigger.ProcessingTime("35 seconds"))
       	.start()
-      
+    
       query.awaitTermination()
 
     }
