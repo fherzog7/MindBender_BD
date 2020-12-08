c60d5d6f7eda66a59abc733f42ad977069564145
fherzog7 <frederick.herzog@enhanceit.us>
2020-12-08 15:46:36 -0500

    "Task-022"

---
+++ CAPSTONE/producer.py
@@ -0,0 +1,90 @@
+from kafka import KafkaProducer
+from time import sleep
+from requests import request 
+from argparse import ArgumentParser
+from geopy.geocoders import Nominatim
+from time import sleep
+
+def getApi_Key():
+	with open("key.txt") as f:
+		return f.readline().rstrip()
+
+def get_args():
+	parser = ArgumentParser()
+	parser.add_argument(
+		"--lat", 
+		choices = range(-90,91),
+		default = Atl_coord[0],
+		type = int,
+		metavar="[-90-90]",
+		help = "Include a specific latitude")
+
+	parser.add_argument(
+		"--lon",
+		choices = range(-180,181),
+		default = Atl_coord[1],
+		type = int,
+		metavar="[-180-180]",
+		help = "Include a specific longitude")
+
+	parser.add_argument(
+		"--dist",
+		choices = [1,5,10,25,100,250],
+		default = 250,
+		type = int,
+		help = "Include the radial distance")
+	parser.add_argument(
+		"--loc",
+		default = 1,
+		help = "Include a location i.e. name of a City i.e.: Portugal.")
+
+	args = parser.parse_args()
+
+	return str(args.lat), str(args.lon), str(args.dist), args.loc
+
+#Kafka Topic
+TOPIC = "capTest"
+#Sandbox
+Broker0 = "sandbox-hdp.hortonworks.com:6667"
+#Local
+Broker1 = "localhost:9099"
+#Multiple Brokers
+BROKERS = ["localhost:9092","localhost:9093", "localhost:9094"]
+RUNNING = True
+api_key = getApi_Key()
+#LAT x LON of ~Georgia~
+geolocator = Nominatim(user_agent="Aircraft")
+Atl = geolocator.geocode("Atlanta, Georgia")
+Atl_coord = [Atl.latitude, Atl.longitude]
+
+lat, lon, distance, loc = get_args()
+if loc != 1:
+	location = geolocator.geocode(loc)
+	lat, lon = str(location.latitude), str(location.longitude)
+else:
+	latlon = lat+', '+lon
+	location = geolocator.reverse(latlon).address
+
+print("Getting ADSB data within a (("+distance+" Mile)) Radius of :: "+str(location))
+
+#Kafka Producer Object
+producer = KafkaProducer(bootstrap_servers = Broker1)
+
+url = "https://adsbexchange-com1.p.rapidapi.com/json/lat/"+lat+"/lon/"+lon+"/dist/"+distance+"/"
+
+headers = {
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': "adsbexchange-com1.p.rapidapi.com"
+    }
+
+while RUNNING:
+	sleep(2)
+	resp = request("GET", url, headers=headers).text
+	print(resp)
+	producer.send(TOPIC, resp.encode('utf-8'))
+	print("Message Sent.")
+	sleep(10)
+	print()
+	print("Message sent to Kafka.")
+
+
